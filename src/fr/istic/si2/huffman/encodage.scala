1127c016ec87533dd0f7bec5137ca3ee23e6c4ef
WaBtey <73140258+WaBtey@users.noreply.github.com>
2021-04-20 20:02:06 +0200

    Update encodage.scala

--- src/fr/istic/si2/huffman/encodage.scala
+++ src/fr/istic/si2/huffman/encodage.scala
@@ -1,49 +1,89 @@
 package fr.istic.si2.huffman
 
-import scala.io.Source
-import java.io.{ File, PrintWriter }
+import Utils._
+import ConstructionCode._
 
-object Utils {
+object Encodage {
 
   /**
-   * @param l une liste de bits
-   * @return la chaîne de 0 et 1 où chaque bit de l est représenté par 0 ou 1, dans l'ordre
+   * @param c un caractère
+   * @param h un arbre de Huffman
+   * @return l'encodage de c, selon h (si c est bien présent dans h)
    */
-  def listBitToString(l: List[Bit]): String = {
+  def encodeSymbol(c: Char, h: Huffman): Option[List[Bit]] = {
+    h match {
+      case Feuille(_ , char)  =>
+        if (c==char) Some(Nil)
+        else None
+      case Noeud(_, a0, a1)   =>
+        encodeSymbol(c, a0) match {
+          case Some(list)     => Some(Zero::list)
+          case None           => 
+            encodeSymbol(c, a1) match {
+              case Some(list) => Some(One::list)
+              case None       => None
+            }
+        }
+    }
+  }
+  
+  /**
+   * @param l une liste de caractères
+   * @param h un arbre de Huffman
+   * @return la séquence de bits correspondants à
+   *         l'encodage selon h des éléments de l, s'il a réussi.
+   *         Les caractères pour lesquels l'encodage est impossible sont oubliés
+   */
+  def encodeList(l: List[Char], h: Huffman): List[Bit] = {
     l match {
-      case Nil          => ""
-      case One :: tail  => "1" + listBitToString(tail)
-      case Zero :: tail => "0" + listBitToString(tail)
+      case Nil            => Nil
+      case head::tail     =>   //encodeSymbol(head, h)::encodeList(tail, h)
+        encodeSymbol(head, h) match {
+          case None       => encodeList(tail, h)
+                        //=> Nil si char impossible la List[Bit] se stoppe au dernier char possible encodé 
+          case Some(list) => list ++ encodeList(tail, h)
+        } 
     }
   }
 
   /**
    * @param s une chaîne de caractères
-   * @return la chaîne de 0 et 1 représentant chaque caractère
-   *         de s par son encodage sur 16 bits
+   * @param h un arbre de Huffman
+   * @return l'encodage de s, selon h, en une liste de bits.
+   *         (concaténation de l'encodage de chaque caractère de s selon h)
    */
-  def vers16Bits(s: String): String = {
-    s.toList.map(c => String.format("%16s", c.toBinaryString).replace(' ', '0')).foldLeft("")((acc, e) => acc + e)
+  def encode(s: String, h: Huffman): List[Bit] = encodeList(s.toList, h)
+
+  /**
+   * @param h un arbre de Huffman
+   * @return une chaîne de 0 et 1 uniquement représentant l'arbre h (voir partie 1.3 de l'énoncé)
+   *         Les caractères encodables avec h sont représentés dans leur encodage binaire 16 bits.
+   */
+  def descriptionHuffman(h: Huffman): String = {
+    h match {
+      case Feuille(_, c)  => "0" + vers16Bits(c.toString)
+      case Noeud(_, g, d) => "1" + descriptionHuffman(g) + descriptionHuffman(d)
+    }
   }
 
   /**
-   * Lit le contenu d'un fichier sur disque.
-   *
-   * @param nom le nom d'un fichier
-   * @return la chaîne contenue dans le fichier nommé nom
-   *
-   * @note Le nom de fichier peut être indiqué de manière
-   *       relative à la racine du projet courant.
+   * @param message une chaîne de caractères
+   * @return la chaîne de 0 et 1, contenant:
+   *         - la représentation de l'arbre de huffman construit à partir de message
+   *         - puis l'encodage de message en utilisant l'arbre construit à partir de message
    */
-  def lireFichier(nom: String): String = {
-    val bufferedSource = Source.fromFile(nom)
-    val contenu = bufferedSource.getLines.mkString(sys.props("line.separator"))
-    bufferedSource.close()
-    contenu
+  def encode(message: String): String = {
+    val h: Huffman = codeHuffman(analyseFrequences(message))
+    descriptionHuffman(h) + listBitToString(encode(message, h))
   }
 
-  /**
-   * Ecrit une chaîne de caractères dans un fichier.
+}
+
+
+
+
+
+
    * Le fichier est écrasé s'il était déjà existant.
    *
    * @param nom le nom du fichier dans lequel on écrit
