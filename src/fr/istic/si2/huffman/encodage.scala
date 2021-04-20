d3dda054e1d44ca52f20855799b7f20393dadf63
WaBtey <73140258+WaBtey@users.noreply.github.com>
2021-04-20 20:00:48 +0200

    Update encodage.scala

--- src/fr/istic/si2/huffman/encodage.scala
+++ src/fr/istic/si2/huffman/encodage.scala
@@ -12,21 +12,21 @@ object Encodage {
    */
   def encodeSymbol(c: Char, h: Huffman): Option[List[Bit]] = {
     h match {
-      case Feuille(_ , char) =>
+      case Feuille(_ , char)  =>
         if (c==char) Some(Nil)
         else None
-      case Noeud(_, a0, a1) =>
+      case Noeud(_, a0, a1)   =>
         encodeSymbol(c, a0) match {
-          case Some(list) => Some(Zero::list)
-          case None => 
+          case Some(list)     => Some(Zero::list)
+          case None           => 
             encodeSymbol(c, a1) match {
               case Some(list) => Some(One::list)
-              case None => None
+              case None       => None
             }
         }
     }
   }
-
+  
   /**
    * @param l une liste de caractères
    * @param h un arbre de Huffman
@@ -36,14 +36,50 @@ object Encodage {
    */
   def encodeList(l: List[Char], h: Huffman): List[Bit] = {
     l match {
-      case Nil => Nil
-      case head::tail =>   //encodeSymbol(head, h)::encodeList(tail, h)
+      case Nil            => Nil
+      case head::tail     =>   //encodeSymbol(head, h)::encodeList(tail, h)
         encodeSymbol(head, h) match {
-          case None => Nil //si char impossible la List[Bit] se stoppe au dernier char possible encodé 
-          case Some(list) => list++encodeList(tail, h)
-        }
+          case None       => encodeList(tail, h)
+                        //=> Nil si char impossible la List[Bit] se stoppe au dernier char possible encodé 
+          case Some(list) => list ++ encodeList(tail, h)
+        } 
     }
   }
+
+  /**
+   * @param s une chaîne de caractères
+   * @param h un arbre de Huffman
+   * @return l'encodage de s, selon h, en une liste de bits.
+   *         (concaténation de l'encodage de chaque caractère de s selon h)
+   */
+  def encode(s: String, h: Huffman): List[Bit] = encodeList(s.toList, h)
+
+  /**
+   * @param h un arbre de Huffman
+   * @return une chaîne de 0 et 1 uniquement représentant l'arbre h (voir partie 1.3 de l'énoncé)
+   *         Les caractères encodables avec h sont représentés dans leur encodage binaire 16 bits.
+   */
+  def descriptionHuffman(h: Huffman): String = {
+    h match {
+      case Feuille(_, c)  => "0" + vers16Bits(c.toString)
+      case Noeud(_, g, d) => "1" + descriptionHuffman(g) + descriptionHuffman(d)
+    }
+  }
+
+  /**
+   * @param message une chaîne de caractères
+   * @return la chaîne de 0 et 1, contenant:
+   *         - la représentation de l'arbre de huffman construit à partir de message
+   *         - puis l'encodage de message en utilisant l'arbre construit à partir de message
+   */
+  def encode(message: String): String = {
+    val h: Huffman = codeHuffman(analyseFrequences(message))
+    descriptionHuffman(h) + listBitToString(encode(message, h))
+  }
+
+}
+
+
 
   /**
    * @param s une chaîne de caractères
