7dbba94509c42f70432681420e341a63b8717c7b
WaBtey <73140258+WaBtey@users.noreply.github.com>
2021-04-20 20:01:05 +0200

    Update encodage.scala

--- src/fr/istic/si2/huffman/encodage.scala
+++ src/fr/istic/si2/huffman/encodage.scala
@@ -1,49 +1,92 @@
 package fr.istic.si2.huffman
 
-import Utils._
-import ConstructionCode._
+import scala.io.Source
+import java.io.{ File, PrintWriter }
 
-object Encodage {
+object Utils {
 
   /**
-   * @param c un caractère
-   * @param h un arbre de Huffman
-   * @return l'encodage de c, selon h (si c est bien présent dans h)
+   * @param l une liste de bits
+   * @return la chaîne de 0 et 1 où chaque bit de l est représenté par 0 ou 1, dans l'ordre
    */
-  def encodeSymbol(c: Char, h: Huffman): Option[List[Bit]] = {
-    h match {
-      case Feuille(_ , char)  =>
-        if (c==char) Some(Nil)
-        else None
-      case Noeud(_, a0, a1)   =>
-        encodeSymbol(c, a0) match {
-          case Some(list)     => Some(Zero::list)
-          case None           => 
-            encodeSymbol(c, a1) match {
-              case Some(list) => Some(One::list)
-              case None       => None
-            }
-        }
+  def listBitToString(l: List[Bit]): String = {
+    l match {
+      case Nil          => ""
+      case One :: tail  => "1" + listBitToString(tail)
+      case Zero :: tail => "0" + listBitToString(tail)
     }
   }
-  
+
   /**
-   * @param l une liste de caractères
-   * @param h un arbre de Huffman
-   * @return la séquence de bits correspondants à
-   *         l'encodage selon h des éléments de l, s'il a réussi.
-   *         Les caractères pour lesquels l'encodage est impossible sont oubliés
+   * @param s une chaîne de caractères
+   * @return la chaîne de 0 et 1 représentant chaque caractère
+   *         de s par son encodage sur 16 bits
    */
-  def encodeList(l: List[Char], h: Huffman): List[Bit] = {
-    l match {
-      case Nil            => Nil
-      case head::tail     =>   //encodeSymbol(head, h)::encodeList(tail, h)
-        encodeSymbol(head, h) match {
-          case None       => encodeList(tail, h)
-                        //=> Nil si char impossible la List[Bit] se stoppe au dernier char possible encodé 
-          case Some(list) => list ++ encodeList(tail, h)
-        } 
+  def vers16Bits(s: String): String = {
+    s.toList.map(c => String.format("%16s", c.toBinaryString).replace(' ', '0')).foldLeft("")((acc, e) => acc + e)
+  }
+
+  /**
+   * Lit le contenu d'un fichier sur disque.
+   *
+   * @param nom le nom d'un fichier
+   * @return la chaîne contenue dans le fichier nommé nom
+   *
+   * @note Le nom de fichier peut être indiqué de manière
+   *       relative à la racine du projet courant.
+   */
+  def lireFichier(nom: String): String = {
+    val bufferedSource = Source.fromFile(nom)
+    val contenu = bufferedSource.getLines.mkString(sys.props("line.separator"))
+    bufferedSource.close()
+    contenu
+  }
+
+  /**
+   * Ecrit une chaîne de caractères dans un fichier.
+   * Le fichier est écrasé s'il était déjà existant.
+   *
+   * @param nom le nom du fichier dans lequel on écrit
+   * @param contenu la chaîne de caractères à écrire
+   *
+   * @note Le nom de fichier peut être indiqué de manière
+   *       relative à la racine du projet courant.
+   */
+  def ecrireFichier(nom: String, contenu: String): Unit = {
+    val writer = new PrintWriter(new File(nom))
+    writer.write(contenu)
+    writer.close()
+  }
+
+  /**
+   * @param s une chaîne de 0 et 1, encodage binaire 16 bits d'un caractère
+   * @return le caractère correspondant à s
+   */
+  def toChar(s: String): Char = {
+    Integer.parseInt(s, 2).toChar
+  }
+
+  /**
+   * @param c un caractère 0 ou 1
+   * @return le bit correspondant à c
+   */
+  def charToBit(c: Char): Bit = {
+    c match {
+      case '0' => Zero
+      case '1' => One
+      case _   => sys.error("Unknown bit character: " + c)
     }
+  }
+
+  /**
+   * @param s une chaîne de 0 et 1 uniquement
+   * @return la liste de bits correspondant à s
+   */
+  def stringToListBit(s: String): List[Bit] = {
+    s.toList.map(charToBit)
+  }
+
+}
   }
 
   /**
